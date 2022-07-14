2156480556d77ab6fb1b4d8f540571559859c1fb
Александра Мурашко <germanalexandera@yandex.ru>
2022-07-14 14:04:19 +0300

    Добавьте файлы проекта.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 100; // начальный счет
+        int add = 1000; // сумма пополнения
+        int bonus = add / 100; // бонус за каждые полные 100 рублей пополнения
+
+        System.out.println("Итоговая сумма на счету:");
+        if (add >= 1000) {
+            System.out.println(account + add + bonus);
+        } else {
+            System.out.println(account + add);
+        }
+
+        System.out.println("Бонусных баллов:");
+        if (add >= 1000) {
+            System.out.println(bonus);
+
+        } else {
+            System.out.println("Бонусов нет");
+
+        }
+    }
+}
+
