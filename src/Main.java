bdae986664ed1ccb630f8cd10fb90f1f2fe7393d
KiraLaird <107937638+KiraLaird@users.noreply.github.com>
2022-07-14 19:44:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         int bonus = add / 100; // бонус за каждые полные 100 рублей пополнения
 
         System.out.println("Итоговая сумма на счету:");
-        if (add >= 1000) {
+        if (add > 1000) {
             System.out.println(account + add + bonus);
         } else {
             System.out.println(account + add);
         }
 
         System.out.println("Бонусных баллов:");
-        if (add >= 1000) {
+        if (add > 1000) {
             System.out.println(bonus);
 
         } else {
